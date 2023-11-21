db1f195cfb3d2276111da21e735794109620d663
kaminm <kaminibarmanm@gmail.com>
2023-11-21 23:07:20 +0530

    21-11-2023 upskill custom properties injection

--- src/main/java/com/springboot/demo/springApp/rest/FunRestController.java
+++ src/main/java/com/springboot/demo/springApp/rest/FunRestController.java
@@ -1,11 +1,26 @@
 package com.springboot.demo.springApp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
 
+    // inject properties for: coach.name and team,name
+
+    @Value("${coach.name}")
+    private  String coachName;
+    @Value("${team.name}")
+    private String teamName;
+
+    //expose new endpoint for "teaminfo"
+    @GetMapping("/teaminfo")
+    public String getTeamInfo(){
+        return  "Coach: " + coachName + " , Team name: " + teamName;
+    }
+
+    // expose "/" that return "Hello world"
     @GetMapping("/")
     public String sayHello(){
         return "Hello world";
